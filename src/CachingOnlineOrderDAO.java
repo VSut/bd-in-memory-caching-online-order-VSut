3e227639762130f25e91b2432ab809eadae4e419
VSut <vaino.sutinen@gmail.com>
2024-08-02 02:04:38 -0500

    passes test

--- src/CachingOnlineOrderDAO.java
+++ src/CachingOnlineOrderDAO.java
@@ -1,16 +1,22 @@
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class CachingOnlineOrderDAO {
 
-
+    private final LoadingCache<String, List<Order>> cache;
     /**
      * Constructor.
      * PARTICIPANTS: Instantiate a LoadingCache instance
      * @param ordersDAO OnlineOrdersDAO that will be used by the cache to retrieve a miss.
      */
     public CachingOnlineOrderDAO(OnlineOrdersDAO ordersDAO) {
-
+        cache = CacheBuilder.newBuilder()
+                .maximumSize(100)
+                .build(CacheLoader.from(ordersDAO::getOrdersByUser));
     }
 
     /**
@@ -20,6 +26,6 @@ public class CachingOnlineOrderDAO {
      * @return List of orders
      */
     public List<Order> getOrdersByUser(String userId) {
-        return new ArrayList<Order>();
+        return cache.getUnchecked(userId);
     }
 }
